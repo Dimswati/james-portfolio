1f1f875b6878f7c49739b359eb9f0305e4a9ff5b
saasplat <141350387+saasplat@users.noreply.github.com>
2023-08-11 08:53:31 +0000

    key prop

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -30,49 +30,9 @@ const FAQSection: React.FC<FAQSectionProps> = ({ faqs }) => {
             <button className="medium-btn-outline-light text-center w-fit mx-auto mb-32"><a href="tel:+254115152238" className="w-full block">Call me now</a></button>
           </div>
           <div className="lg:max-w-[75vw] mx-auto">
-            {/* <div className="mb-12">
-              <div className="bg-neutral-100 h-[1px] w-full"></div>
-              <div className="flex justify-between gap-x-6 items-center text-2xl text-neutral-100 my-12">
-                <h4 className="capitalize">Is there a down payment</h4>
-                <span><AiOutlineMinus/></span>
-              </div>
-              <p className="text-neutral-200 text-lg font-light">Depending on which you like, you can decide either to bring your own materials or live the entire process to me</p>
-            </div> */}
             {faqList.map(faq => (
-              <FAQ faq={faq.node} show={currentFaqId === faq.node.id ? true : false} setCurrentFaqId={setCurrentFaqId}/>
+              <FAQ key={faq.node.id} faq={faq.node} show={currentFaqId === faq.node.id ? true : false} setCurrentFaqId={setCurrentFaqId}/>
             ))}
-            {/* <div className="mb-12">
-              <div className="bg-neutral-100 h-[1px] w-full"></div>
-              <div className="flex justify-between gap-x-6 items-center text-2xl text-neutral-100 my-12">
-                <h4 className="capitalize">Is there a down payment</h4>
-                <span><AiOutlinePlus/></span>
-              </div>
-              <p className="text-neutral-200 text-lg font-light hidden">Depending on which you like, you can decide either to bring your own materials or live the entire process to me</p>
-            </div>
-            <div className="mb-12">
-              <div className="bg-neutral-100 h-[1px] w-full"></div>
-              <div className="flex justify-between gap-x-6 items-center text-2xl text-neutral-100 my-12">
-                <h4 className="capitalize">Is there a down payment</h4>
-                <span><AiOutlinePlus/></span>
-              </div>
-              <p className="text-neutral-200 text-lg font-light hidden">Depending on which you like, you can decide either to bring your own materials or live the entire process to me</p>
-            </div>
-            <div className="mb-12">
-              <div className="bg-neutral-100 h-[1px] w-full"></div>
-              <div className="flex justify-between gap-x-6 items-center text-2xl text-neutral-100 my-12">
-                <h4 className="capitalize">Is there a down payment</h4>
-                <span><AiOutlinePlus/></span>
-              </div>
-              <p className="text-neutral-200 text-lg font-light hidden">Depending on which you like, you can decide either to bring your own materials or live the entire process to me</p>
-            </div>
-            <div className="mb-12">
-              <div className="bg-neutral-100 h-[1px] w-full"></div>
-              <div className="flex justify-between gap-x-6 items-center text-2xl text-neutral-100 my-12">
-                <h4 className="capitalize">Is there a down payment</h4>
-                <span><AiOutlinePlus/></span>
-              </div>
-              <p className="text-neutral-200 text-lg font-light hidden">Depending on which you like, you can decide either to bring your own materials or live the entire process to me</p>
-            </div> */}
           </div>
         </div>
       </section>
